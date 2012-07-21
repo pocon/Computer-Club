bfe0eb0f607ea211a584dd6d02e9f2e860ed31f6
Patrick O'Connell <paddymelon@padmel.com>
2012-07-22 01:27:54 +1000

    Added input alternative

--- Week 1/Exercises/q3.py
+++ Week 1/Exercises/q3.py
@@ -3,3 +3,9 @@ int2=int(raw_input("Enter the second integer: "))
 print int1+int2
 
 #Note that this program doesn't check if the inputted values are actually integer, it'll throw an error if not by default design of the int() function
+
+# Alternative using input, rather than raw input:
+
+int1 = input("Enter the First Integer: ")
+int2 = input("Enter the Second Integer: ")
+print int1 + int2
