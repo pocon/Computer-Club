42ef0976f4ec8e395536515e3ca5c4f4ad9aa933
Patrick O'Connell <paddymelon@padmel.com>
2012-07-22 01:36:04 +1000

    Added One-Line Solution

--- Week 1/Exercises/q6.py
+++ Week 1/Exercises/q6.py
@@ -5,3 +5,7 @@ while c<len(string):
  acc+=string[len(string)-c-1]
  c+=1
 print acc
+
+# One Line Solution Using the Fact that string[start:end:step] and a bit of hax:
+
+print raw_input("Enter the String: ")[::-1]
