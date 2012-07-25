9caa7e3fe26180717e25208fb45c352b33d4caec
Patrick O'Connell <paddymelon@padmel.com>
2012-07-25 18:51:48 +1000

    Added len explanation

--- Week 1/Exercises/q6.py
+++ Week 1/Exercises/q6.py
@@ -1,7 +1,7 @@
 string=raw_input("Enter the string: ")
 acc=""
 c=0
-while c<len(string):
+while c<len(string): # len is short for length and counts the number of characters in the string (letters, numbers and symbols)
  acc+=string[len(string)-c-1]
  c+=1
 print acc
