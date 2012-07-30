0bb194dbea5468d7d0d2902ba90888d7927fa314
Patrick O'Connell <paddymelon@padmel.com>
2012-07-30 22:57:12 +1000

    Updated to New Winner

--- Week 1/Exercises/Prime_number_test.py
+++ Week 1/Exercises/Prime_number_test.py
@@ -1,7 +1,8 @@
-# Evan Hockings
-n = input('');  r = 'Prime'
-if n < 2:   r = 'Not prime'
-else: 
-	for i in range(2,n): 
-		if n % i == 0:  r = 'Not prime'
-print r
+#mokhaccino 
+n = input("Type in a Number to see if it is Prime ")
+while True: 
+	for i in range(2,n):
+		if n%i ==0:
+			exit("composite")
+	else:	 
+		exit("prime")
