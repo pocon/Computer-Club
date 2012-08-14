d9d8da09b9254c3cbb6ac9f2f1e525afbd6f1a11
Will <willw53@fastmail.fm>
2012-08-14 13:28:48 +1000

    haven't learnt format operator

--- Week 4/Exercises/easy.py
+++ Week 4/Exercises/easy.py
@@ -1,9 +1,9 @@
 bottles_left = 99
 
 while bottles_left != 1:
-    print "%d bottles of beer on the wall, %d bottles of beer." % (bottles_left, bottles_left)
+    print str(bottles_left)+" bottles of beer on the wall, "+str(bottles_left)+" bottles of beer."
     bottles_left -= 1
-    print "Take one down and pass it around, %d bottles of beer on the wall." % bottles_left
+    print "Take one down and pass it around, "+str(bottles_left)+" bottles of beer on the wall."
 
 print "1 bottle of beer on the wall, 1 bottle of beer."
 print "Take one down and pass it around, no more bottles of beer on the wall."
